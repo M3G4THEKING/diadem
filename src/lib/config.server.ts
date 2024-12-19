a2fcd2e729b47418e74b9843f0385d3985abbcf2
Malte <42342921+ccev@users.noreply.github.com>
2024-12-19 18:09:10 +0100

    fix last commit

--- src/lib/config.server.ts
+++ src/lib/config.server.ts
@@ -1,31 +1,22 @@
 import { parse } from 'toml';
 import type { Config } from '@/lib/types/config';
 import { env } from '$env/dynamic/private';
+import type { Asset } from '@sveltejs/kit/src/types/internal';
 
 let config: Config;
 
 export async function readConfig() {
-	let configFile;
-	let read: ((asset: string) => Response) | undefined;
 	let rawToml: string = '';
+	let configFile
 
 	try {
-		const server = await import('$app/server');
-		read = server.read;
-	} catch (e) {
-		rawToml = env.CONFIG_TOML ?? '';
+		configFile = await import('./server/config.toml?raw')
+	} catch(e) {
+		console.error('Please copy config.example.toml to config.toml! Starting with default config')
+		configFile = await import('./server/config.example.toml?raw')
 	}
 
-	if (read) {
-		try {
-			configFile = await import('./server/config.toml?raw');
-		} catch (e) {
-			console.error('Please copy config.example.toml to config.toml! Starting with default config');
-			configFile = await import('./server/config.example.toml?raw');
-		}
-
-		rawToml = configFile.default;
-	}
+	rawToml = configFile.default
 
 	config = parse(rawToml);
 	return config.client;
