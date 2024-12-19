8273e9d6651e640a489d4281ba54deacc9700709
Malte <42342921+ccev@users.noreply.github.com>
2024-12-19 17:57:52 +0100

    don't rely on node to read config

--- src/lib/config.server.ts
+++ src/lib/config.server.ts
@@ -1,27 +1,36 @@
-import {parse} from "toml"
-import {readFile} from 'node:fs/promises'
-import type {Config} from '@/lib/types/config';
+import { parse } from 'toml';
+import type { Config } from '@/lib/types/config';
+import { env } from '$env/dynamic/private';
 
-let config: Config
-
-async function readName(name: string){
-	return await readFile(name, "utf-8")
-}
+let config: Config;
 
 export async function readConfig() {
-	let rawToml: string
+	let configFile;
+	let read: ((asset: string) => Response) | undefined;
+	let rawToml: string = '';
 
 	try {
-		rawToml = await readName("config.toml")
-	} catch(e) {
-		console.error("Please copy config.example.toml to config.toml! Starting with default config")
-		rawToml = await readName("config.example.toml")
+		const server = await import('$app/server');
+		read = server.read;
+	} catch (e) {
+		rawToml = env.CONFIG_TOML ?? '';
 	}
 
-	config = parse(rawToml)
-	return config.client
+	if (read) {
+		try {
+			configFile = await import('./server/config.toml?raw');
+		} catch (e) {
+			console.error('Please copy config.example.toml to config.toml! Starting with default config');
+			configFile = await import('./server/config.example.toml?raw');
+		}
+
+		rawToml = configFile.default;
+	}
+
+	config = parse(rawToml);
+	return config.client;
 }
 
 export function getServerConfig() {
-	return config.server
+	return config.server;
 }
